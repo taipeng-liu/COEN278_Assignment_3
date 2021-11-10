b47843f4ff07442710718b3576666090b12e14cb
taipeng <lyutiepeng@gmail.com>
2021-11-10 13:15:31 -0800

    update

--- Gemfile
+++ Gemfile
@@ -1,18 +1,20 @@
 source 'https://rubygems.org'
 
-ruby "2.7.4"
+ruby "2.7.0"
 
 gem 'sinatra'
 gem 'slim'
-gem 'dm-core'
-gem 'dm-migrations'
 gem 'thin'
-gem 'pg'
-gem 'dm-postgres-adapter'
 gem 'stripe'
 gem 'json'
 gem 'dotenv'
 gem 'activerecord'
-gem 'mysql2', :group => :development
-
 gem 'sinatra-reloader'
+
+group :development do
+  gem 'mysql2'
+end
+
+group :production do
+    gem 'pg'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,18 @@
 require 'active_record'
 
+if development?
+    ActiveRecord::Base.establish_connection(
+        :adapter  => "mysql2",
+        :host     => "127.0.0.1",
+        :port     => "3306",
+        :username => "taipeng",
+        :password => "coen380",
+        :database => "coen278"
+    )
+else
+    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+end
+
 ActiveRecord::Migration.create_table :users do |t|
     t.string :name
     t.string :password
