044350573fc30dfe4d2b0cd1e37b163ff8fdf837
taipeng <lyutiepeng@gmail.com>
2021-11-10 13:25:19 -0800

    add pg

--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,7 @@ require 'sinatra'
 require 'rubygems'
 require 'active_record'
 
-if development?
-    ActiveRecord::Base.establish_connection(
-        :adapter  => "mysql2",
-        :host     => "127.0.0.1",
-        :port     => "3306",
-        :username => "taipeng",
-        :password => "coen380",
-        :database => "coen278"
-    )
-else
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-end
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
 ActiveRecord::Migration.create_table :users do |t|
     t.string :name
